cbfb795a0ed248a36788c0e207b8bc954caae424
Djzet <silaevegor2003@mail.ru>
2022-10-12 14:10:00 +0700

    add registration

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 
 # Create your views here.
 
+
 def index(request):
     """
     Функция отображения для домашней страницы сайта.
@@ -16,27 +17,29 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
     num_genre = Genre.objects.count()
-    # num_book = Book.objects..count()
+    num_book = Book.objects.filter(title__icontains='Тарас Бульба').count()
+    # Количество посещений этого представления, подсчитанное в переменной сеанса.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
-    # Отрисовка HTML-шаблона index.html с данными внутри
-    # переменной контекста context
+    # Визуализация HTML-шаблона index.html с данными в контекстной переменной.
     return render(
         request,
         'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors,
-                 'num_genre': num_genre},
+                 'num_genre': num_genre, 'num_book': num_book, 'num_visits': num_visits},  # num_visits appended
     )
 
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 10
+    paginate_by = 2
     context_object_name = 'book_list'  # ваше собственное имя переменной контекста в шаблоне
     template_name = 'books/book_list.html'  # Определение имени вашего шаблона и его расположения
 
     def get_queryset(self):
-        return Book.objects.filter(title__icontains='war')[:5]  # Получить 5 книг, содержащих 'war' в заголовке
+        return Book.objects.filter(title__icontains='')  # [:5]  # Получить 5 книг, содержащих 'war' в заголовке
 
     def get_context_data(self, **kwargs):
         # В первую очередь получаем базовую реализацию контекста
@@ -48,3 +51,25 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+    context_object_name = 'author_list'  # ваше собственное имя переменной контекста в шаблоне
+    template_name = 'authors/author_list.html'  # Определение имени вашего шаблона и его расположения
+
+    def get_queryset(self):
+        # return Author.objects.filter(last_name__icontains='')  # [:5]  # Получить 5 книг, содержащих 'war' в заголовке
+        return Author.objects.all()
+
+    def get_context_data(self, **kwargs):
+        # В первую очередь получаем базовую реализацию контекста
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os  # требуется по коду ниже
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
-    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +54,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -115,7 +110,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -125,3 +119,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_SAVE_EVERY_REQUEST = True
+
+# Перенаправление на домашний URL после входа в систему (по умолчанию перенаправляется на /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = 'logged_out'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
