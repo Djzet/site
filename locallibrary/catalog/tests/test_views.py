0a88f1b0322e9cad59a54d6cd66eb0ccb131dcb8
Djzet <silaevegor2003@mail.ru>
2022-10-13 14:05:54 +0700

    add tests

--- locallibrary/catalog/tests/test_views.py
+++ locallibrary/catalog/tests/test_views.py
@@ -14,8 +14,8 @@ class AuthorListViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        # Создайте 3 авторов для тестов разбиения на страницы
-        number_of_authors = 3
+        # Создайте 20 авторов для тестов разбиения на страницы
+        number_of_authors = 20
         for author_num in range(number_of_authors):
             Author.objects.create(first_name='Christian %s' % author_num, last_name='Surname %s' % author_num, )
 
@@ -41,12 +41,12 @@ class AuthorListViewTest(TestCase):
         self.assertTrue(len(resp.context['author_list']) == 10)
 
     def test_lists_all_authors(self):
-        # Get second page and confirm it has (exactly) remaining 3 items
+        # Get second page and confirm it has (exactly) remaining 20 items
         resp = self.client.get(reverse('authors') + '?page=2')
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
-        self.assertTrue(len(resp.context['author_list']) == 3)
+        self.assertTrue(len(resp.context['author_list']) == 10)
 
 
 class LoanedBookInstancesByUserListViewTest(TestCase):
